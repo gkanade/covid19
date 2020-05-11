efcb106bbfdcaad3d8cd3cb2093ed5965210a674
gkanade <gkanade@GAURAV-4881>
2020-05-11 16:14:05 +0530

    Add project files.

---
+++ ConvertCOVIDStatewiseDailyJsonToCsv/Program.cs
@@ -0,0 +1,121 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertCOVIDStatewiseDailyJsonToCsv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls
+        | SecurityProtocolType.Tls11
+        | SecurityProtocolType.Tls12
+        | SecurityProtocolType.Ssl3;
+            using (var client = new WebClient())
+            {
+                client.DownloadFile("https://api.covid19india.org/states_daily.json", "states_daily.json");
+            }
+            JObject o1 = JObject.Parse(File.ReadAllText("states_daily.json"));
+            IDictionary<string, List<int>> dictFatality = new Dictionary<string, List<int>>();
+            IDictionary<string, List<int>> dictConfirmed = new Dictionary<string, List<int>>();
+            List<string> states = new List<string>{ "an", "ap", "ar", "as",
+            "br", "ch", "ct", "dd", "dl", "dn", "ga", "gj",
+            "hp", "hr", "jh", "jk", "ka", "kl", "la", "ld",
+            "mh", "ml", "mn", "mp", "mz", "nl", "or", "pb",
+            "py", "rj", "sk", "tg", "tn", "tr", "up", "ut", "wb"};
+
+            foreach(string state in states)
+            {
+                dictFatality.Add(state, new List<int>());
+                dictConfirmed.Add(state, new List<int>());
+            }
+            
+            int numEntries = o1["states_daily"].Count();
+            for(int i = 0; i < numEntries; i++)
+            {
+                var entry = o1["states_daily"][i];
+                if(entry["status"].ToString().Equals("Deceased"))
+                {
+                    foreach(string state in states)
+                    {
+                        int currentEntry = entry[state].ToString() == "" ? 0 : int.Parse(entry[state].ToString());
+                        if(dictFatality[state].Count() == 0)
+                        {
+                            dictFatality[state].Add(currentEntry);
+                        }
+                        dictFatality[state].Add(currentEntry + int.Parse(dictFatality[state].Last().ToString()));
+                    }
+                    
+                }
+                if (entry["status"].ToString().Equals("Confirmed"))
+                {
+                    foreach (string state in states)
+                    {
+                        int currentEntry = entry[state].ToString() == "" ? 0 : int.Parse(entry[state].ToString());
+                        if (dictConfirmed[state].Count() == 0)
+                        {
+                            dictConfirmed[state].Add(currentEntry);
+                        }
+                        dictConfirmed[state].Add(currentEntry + int.Parse(dictConfirmed[state].Last().ToString()));
+                    }
+
+                }
+            }
+
+            List<string> lines = new List<string>();
+            foreach(string key in dictFatality.Keys)
+            {
+                string line = "";
+                Console.Write(key + " ");
+                line=line+(key + ",");
+                foreach(int i in dictFatality[key])
+                {
+                    Console.Write(i + ",");
+                    line=line+(i + ",");
+                }
+                Console.WriteLine();
+                //lines.Add("");
+                lines.Add(line);
+            }
+
+            using (StreamWriter op = new StreamWriter("states_data_deaths"+ DateTime.Now.ToString("yyyyMMddHHmmssfff")+".csv"))
+            {
+                foreach (string line in lines)
+                {
+                    op.WriteLine(line);
+                }
+                op.Flush();
+            }
+            lines = new List<string>();
+            foreach (string key in dictConfirmed.Keys)
+            {
+                string line = "";
+                Console.Write(key + " ");
+                line = line + (key + ",");
+                foreach (int i in dictConfirmed[key])
+                {
+                    Console.Write(i + ",");
+                    line = line + (i + ",");
+                }
+                Console.WriteLine();
+                //lines.Add("");
+                lines.Add(line);
+            }
+
+            using (StreamWriter op = new StreamWriter("states_data_confirmed"+ DateTime.Now.ToString("yyyyMMddHHmmssfff")+".csv"))
+            {
+                foreach (string line in lines)
+                {
+                    op.WriteLine(line);
+                }
+                op.Flush();
+            }
+        }
+    }
+}
